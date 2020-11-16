a3ae55cf6270d27455e48b97f7d0c19502800464
Pheraone <brechje@me.com>
2020-11-16 13:00:38 +0100

    Basic Set-up

---
+++ Kernmodule Development/Assets/Scripts/BoidBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidBehaviour : MonoBehaviour
+{
+    public Vector3 velocity;
+    public float rotationSpeed;
+
+    public void Start()
+    {
+        velocity = Random.insideUnitSphere * 1;
+    }
+
+    public void OnUpdate()
+    {
+        // * Time.deltaTime
+        transform.position += velocity * Time.deltaTime;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(velocity), rotationSpeed * Time.deltaTime);
+    }
+
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+}
---
+++ Kernmodule Development/Assets/Scripts/BoidsManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+
+public class BoidsManager : MonoBehaviour
+{
+
+    public BoidBehaviour boid;
+    public int boidCount;
+    private List<BoidBehaviour> boidList = new List<BoidBehaviour>();
+
+
+    void Start()
+    {
+        //TODO: put al the boids in a list
+        InitializeBoids(boid);
+    }
+
+    void Update()
+    {
+        foreach (BoidBehaviour tempBoid in boidList)
+        {
+            //Rule1(tempBoid);
+            tempBoid.OnUpdate();
+        }
+    }
+
+
+    private void InitializeBoids(BoidBehaviour prefab)
+    {
+        for (int i = 0; i < boidCount; i++)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
+            BoidBehaviour tempBoid = Instantiate(boid, randomPosition, Quaternion.identity);
+
+            tempBoid.name = "boid" + i;
+            boidList.Add(tempBoid);
+        }
+    }
+
+    //Vector3 Rule1(BoidBehaviour boidInstance)
+    //{
+
+    //    Vector3 pos = boidScript.GetPosition();
+    //    avarage position all boids
+    //    foreach (GameObject tempBoid in boidList)
+    //    {
+    //        positionList.Add(pos);
+    //    }
+
+    //    Debug.Log(pos);
+    //    centreOfMass = (all boids) / n
+    //    return pos;
+    //}
+}
+
+
